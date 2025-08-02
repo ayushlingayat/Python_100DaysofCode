187a1eb325114edfe0637e051ce4970de763d360
Ayush Shyam Lingayat <lingayatayush3@gmail.com>
2025-08-02 11:15:33 +0530

    100DaysofPython

---
+++ ImplementStackusingQueues_Day40.py
@@ -0,0 +1,75 @@
+#Brute Approach
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        res = self.q1.popleft()
+        self.q1, self.q2 = self.q2, self.q1
+        return res
+
+    def top(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        res = self.q1.popleft()
+        self.q2.append(res)
+        self.q1, self.q2 = self.q2, self.q1
+        return res
+
+    def empty(self) -> bool:
+        return not self.q1
+
+#Better Approach
+
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return not self.q
+
+#Optimal Approach
+
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())  #my rotation logic 
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return not self.q
