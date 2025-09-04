420a5e891c34cadbd27dac9b41d62fc63e29865d
Ayush Shyam Lingayat <lingayatayush3@gmail.com>
2025-09-04 20:18:38 +0530

    100DaysofPython

---
+++ ConstructBinaryTree_fromInorder&Preorder_Day69.py
@@ -0,0 +1,34 @@
+#Optimal Code
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+# Optimal Approach
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        inorder_map = {val: idx for idx, val in enumerate(inorder)}
+        pre_idx = 0
+
+        def helper(in_left, in_right):
+            nonlocal pre_idx
+            if in_left > in_right:
+                return None
+
+            root_val = preorder[pre_idx]
+            pre_idx += 1
+            root = TreeNode(root_val)
+
+            mid = inorder_map[root_val]
+
+            root.left = helper(in_left, mid - 1)
+            root.right = helper(mid + 1, in_right)
+
+            return root
+
+        return helper(0, len(inorder) - 1)
+
