f3cc2979ee623538e6e4c92820918b22d83159ba
Ayush Shyam Lingayat <lingayatayush3@gmail.com>
2025-09-20 10:38:04 +0530

    100DaysofPython

---
+++ GasStation_Day85.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total_gas = total_cost = 0
+        start_index = 0
+        current_gas = 0
+
+        for i in range(len(gas)):
+            total_gas += gas[i]
+            total_cost += cost[i]
+            current_gas += gas[i] - cost[i]
+
+            # If we run out of gas, move the starting point to i+1
+            if current_gas < 0:
+                start_index = i + 1
+                current_gas = 0
+
+        return start_index if total_gas >= total_cost else -1
+
